3d1cc59e92265cbee407826885d046eaaf89f104
pacter-69 <aldomcorb@gmail.com>
2026-01-22 20:04:07 +0100

    Start animation and change main scene

---
+++ Assets/Scripts/ForegroundAnimation.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ForegroundAnimation : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.Play("Foreground");
+    }
+}
