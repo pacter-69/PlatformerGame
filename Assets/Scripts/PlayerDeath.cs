fd9377e2f1e27f9c07f5981818653d976b9e0903
Pol <pol.cozmo@gmail.com>
2026-01-24 11:19:56 +0100

    Pantalla muerte

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Kill"))
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Kill"))
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
+}
